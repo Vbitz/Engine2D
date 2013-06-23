d42b33c8a3fcb24ceb902479d2460cd37996d739
Vbitz <jscarsbrook@gmail.com>
2013-06-23 20:59:51 +1200

    Yay console improvements.

--- src/EngineUI.cpp
+++ src/EngineUI.cpp
@@ -12,10 +12,18 @@ namespace Engine {
             if (!_active) {
                 return;
             }
-            Draw2D::SetColor(0.1f, 0.1f, 0.1f);
+            if (!_showConsole) {
+                Draw2D::SetColor(25 / 255.0f, 25 / 255.0f, 25 / 255.0f);
+            } else {
+                Draw2D::SetColor(40 / 255.0f, 40 / 255.0f, 40 / 255.0f);
+            }
             Draw2D::Rect(0.0f, 0.0f, getScreenWidth(), _showConsole ? getScreenHeight() : 14);
-            Draw2D::SetFont("monospace", 10);
-            Draw2D::SetColor(1.0f, 1.0f, 1.0f);
+            Draw2D::SetFont("basic", 10);
+            if (!_showConsole) {
+                Draw2D::SetColor(220 / 255.0f, 220 / 255.0f, 220 / 255.0f);
+            } else {
+                Draw2D::SetColor(1.0f, 1.0f, 1.0f);
+            }
             Draw2D::Print(10, 4, "-- Engine2D --");
             ss.str("");
             ss << "DrawTime: " << Profiler::GetTime("Draw");
@@ -24,29 +32,37 @@ namespace Engine {
                 return;
             }
             std::vector<Logger::LogEvent>* logEvents = Logger::GetEvents();
-            int i = 2;
+            int i = 20;
             for (auto iterator = logEvents->rbegin(); iterator < logEvents->rend(); iterator++) {
-                switch (iterator->Level) {
-                    case Logger::LogLevel_Verbose:
-                        Draw2D::SetColor(205 / 255.0f, 205 / 255.0f, 205 / 255.0f);
-                        break;
-                    case Logger::LogLevel_User:
-                        Draw2D::SetColor(34 / 255.0f, 139 / 255.0f, 34 / 255.0f);
-                        break;
-                    case Logger::LogLevel_Log:
-                        Draw2D::SetColor(192 / 255.0f, 192 / 255.0f, 192 / 255.0f);
-                        break;
-                    case Logger::LogLevel_Warning:
-                        Draw2D::SetColor(255 / 255.0f, 165 / 255.0f, 0 / 255.0f);
-                        break;
-                    case Logger::LogLevel_Error:
-                        Draw2D::SetColor(178 / 255.0f, 34 / 255.0f, 34 / 255.0f);
-                        break;
-                }
                 if (iterator->Level == Logger::LogLevel_Verbose && !_showVerbose) {
-                    i += 4; // add some padding to show that a message is there, just hidden
+                    Draw2D::SetColor(80 / 255.0f, 80 / 255.0f, 80 / 255.0f);
+                    Draw2D::Rect(0, i + 2, getScreenWidth(), 2);
+                    i += 6; // add some padding to show that a message is there, just hidden
                 } else {
-                    Draw2D::Print(5, i += 12, iterator->Event.c_str());
+                    Draw2D::SetColor(30 / 255.0f, 30 / 255.0f, 30 / 255.0f);
+                    Draw2D::Rect(60, i + 1, getScreenWidth() - 60, 20);
+                    switch (iterator->Level) {
+                        case Logger::LogLevel_Verbose:
+                            Draw2D::SetColor(205 / 255.0f, 205 / 255.0f, 205 / 255.0f);
+                            break;
+                        case Logger::LogLevel_User:
+                            Draw2D::SetColor(34 / 255.0f, 139 / 255.0f, 34 / 255.0f);
+                            break;
+                        case Logger::LogLevel_Log:
+                            Draw2D::SetColor(192 / 255.0f, 192 / 255.0f, 192 / 255.0f);
+                            break;
+                        case Logger::LogLevel_Warning:
+                            Draw2D::SetColor(255 / 255.0f, 165 / 255.0f, 0 / 255.0f);
+                            break;
+                        case Logger::LogLevel_Error:
+                            Draw2D::SetColor(178 / 255.0f, 34 / 255.0f, 34 / 255.0f);
+                            break;
+                    }
+                    std::string time = std::to_string(iterator->time);
+                    time.resize(6, '0');
+                    Draw2D::Print(5, i + 7, (time + "s").c_str());
+                    Draw2D::Print(65, i + 7, iterator->Event.c_str());
+                    i += 22;
                 }
                 if (i > getScreenHeight()) {
                     break;
