c95598767c964b7b6a9b460073b747b536ab0db3
Vbitz <jscarsbrook@gmail.com>
2014-03-19 23:54:40 +1300

    Added licence header to tasks.py and minor changes

--- tasks.py
+++ tasks.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
-# Engine2D Task Scripts
-# Built for Python 2.7
+#   Filename: tasks.py
+#   Purpose:  Build helper tools for gyp and documentation
+#
+#   Part of Engine2D
+#
+#   Copyright (C) 2014 Vbitz
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 import sys;
 import os;
@@ -192,7 +208,7 @@ def run():
 			"-debug"
 		]);
 
-@command(requires=["build_env"], usage="Runs the engine in Development Mode")
+@command(requires=["build_env"], usage="Runs the engine in Test Mode")
 def test():
 	shell_command([
 			resolve_path(PROJECT_BUILD_PATH, get_exe_name()),
