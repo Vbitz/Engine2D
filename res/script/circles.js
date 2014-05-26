7527b55fd9fcd52a6ccc494b63ff06abb6ea7a5a
Vbitz <jscarsbrook@gmail.com>
2014-05-26 22:13:27 +1200

    Updated res/script/circles.js

--- res/script/circles.js
+++ res/script/circles.js
@@ -1,8 +1,10 @@
 var circles = [];
 
+var ringSize = 2.0;
+
 function* rainbowGradient() {
 	while (true) {
-		for (var x = 0; x < 360; x += 10) {
+		for (var x = 0; x < 360; x += 2) {
 			yield draw.getRGBFromHSV(x, 1.0, 0.5);
 		}
 	}
@@ -10,21 +12,22 @@ function* rainbowGradient() {
 
 var rainbow = rainbowGradient();
 
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 400; i++) {
 	circles.push([0, rainbow.next().value]);
 }
 
 event.draw = function circles_draw(e) {
 	for (var i = 0; i < circles.length; i++) {
 		draw.setColor(circles[i][1]);
-		draw.circle(sys.screenWidth / 2, sys.screenHeight / 2, i * 2, (i - 1) * 2, i * 2, circles[i][0], circles[i][0] + 0.5, true);
+		draw.circle(sys.screenWidth / 2, sys.screenHeight / 2, i * ringSize, (i - 1) * ringSize, 3, circles[i][0], Math.abs(circles[i][0] + Math.sin(i) * 0.1), true);
+		draw.circle(sys.screenWidth / 2, sys.screenHeight / 2, i * ringSize, (i - 1) * ringSize, 3, circles[i][0] + 0.5, Math.abs(circles[i][0] + (Math.sin(i) * 0.1)) + 0.5, true);
 		if (i % 2 === 0) {
-			circles[i][0] += Math.tan(i * 0.01) * sys.deltaTime;
+			circles[i][0] += Math.sin(i * 0.01) * sys.deltaTime * 0.05;
 		} else {
-			circles[i][0] -= Math.tan(i * 0.01) * sys.deltaTime;
+			circles[i][0] -= Math.sin(i * 0.01) * sys.deltaTime * 0.05;
 		}
 		if (circles[i][0] > 1.0) {
-			while (circles[i][0] > 1.0) {
+			while (circles[i][0] > 1.0) {	
 				circles[i][0] -= 1.0;
 			}
 		}
