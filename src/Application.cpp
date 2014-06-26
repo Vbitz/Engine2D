2c5d2516c940168fce1d169f53a6571201d852b2
Vbitz <jscarsbrook@gmail.com>
2014-06-26 17:28:59 +1200

    The config cmdline flag is now really fixed

--- src/Application.cpp
+++ src/Application.cpp
@@ -692,7 +692,7 @@ namespace Engine {
                 size_t valueLength = strlen(argv[i]) - keyLength - 3; // 2 chars for -C and 1 for =
                 
                 char* key = (char*) malloc(keyLength + 1);
-                key[valueLength] = '\0';
+                key[keyLength] = '\0';
                 strncpy(key, &argv[i][2], keyLength);
                 
                 char* value = (char*) malloc(valueLength + 1);
