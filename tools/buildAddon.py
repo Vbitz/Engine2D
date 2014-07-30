a7af98f745a0fc1aa3bcf99b06b731badc73390c
Vbitz <jscarsbrook@gmail.com>
2014-07-30 15:49:40 +1200

    Improved tools/buildAddon.py

--- tools/buildAddon.py
+++ tools/buildAddon.py
@@ -11,21 +11,28 @@ import uuid
 import sys
 import argparse
 
-def compile(sources, outputFilename):
+def compile(sources, outputFilename, link_v8, addedArgs):
 	if outputFilename == None:
 		outputFilename = "addon.dylib"
+	if addedArgs == None:
+		addedArgs = []
 	args = ["clang++", "-dynamiclib", "-std=gnu++11",
-		"-stdlib=libc++", "-lengine2D", "-lv8", "-Lbuild/Default", "-o", outputFilename]
+		"-stdlib=libc++", "-lengine2D", "-Lbuild/Default", "-o", outputFilename]
 	args += ["-D_BUILD_UUID=\"" + str(uuid.uuid4()) + "\""]
+	if link_v8:
+		args += ["-lv8"]
+	args += addedArgs
 	args += sources
 	shell_command(args)
 	shell_command(["install_name_tool", "-change", "/usr/local/lib/libengine2D.dylib",
 		"@executable_path/libengine2D.dylib", outputFilename])
 
-parser = argparse.ArgumentParser(description='')
+parser = argparse.ArgumentParser(description='Build script for Engine2D addons')
 parser.add_argument("sources", nargs='+', help='C++ source files to compile into a library')
-parser.add_argument("--output", help='Output filename')
+parser.add_argument("-o", help='Output filename', metavar='filename')
+parser.add_argument("-a", nargs='+', help='Add additional arguments to the build command-line', metavar='arg')
+parser.add_argument("--link-v8", default=False, help='Links V8', action='store_true')
 
 args = parser.parse_args()
 
-compile(args.sources, args.output)
+compile(args.sources, args.o, args.link_v8, args.a)
