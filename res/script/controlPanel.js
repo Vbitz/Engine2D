0800996f25dd279a3ac8b904dfeff83f6eb2ec0c
Vbitz <jscarsbrook@gmail.com>
2014-08-16 19:56:45 +1200

    Updated res/script/controlPanel.js

--- res/script/controlPanel.js
+++ res/script/controlPanel.js
@@ -30,8 +30,12 @@ function getSpecialChars(key) {
 var currentText = "";
 
 function onInput(raw, key) {
-	if (raw) currentText += key.toLowerCase();
-	else {
+	if (raw) {
+		currentText += key.toLowerCase();
+		controls.forEach(function (control) {
+			control.submitRawInput(key);
+		});
+	} else {
 		if (key === "ESCAPE") {
 			currentText = "";
 		} else if (key === "ENTER") {
@@ -48,14 +52,14 @@ function onInput(raw, key) {
 	}
 }
 
-function ButtonControl(label, x, y, callback) {
+function CommandButtonControl(label, x, y, callback) {
 	this.label = ":" + label;
 	this.x = x;
 	this.y = y;
 	this.callback = callback || function () { };
 }
 
-ButtonControl.prototype.draw = function () {
+CommandButtonControl.prototype.draw = function () {
 	var totalWidth = 100;
 	if (currentText.length > 0 && this.label.indexOf(currentText) === 0) {
 		draw.setColor(0x00ff00);
@@ -69,12 +73,46 @@ ButtonControl.prototype.draw = function () {
 	draw.print(this.x + 10, this.y + 4, this.label);
 };
 
-ButtonControl.prototype.submitInput = function () {
+CommandButtonControl.prototype.submitInput = function () {
 	if (this.label !== currentText) return;
 	this.callback();
 };
 
-ButtonControl.prototype.submitSpecialInput = function () { };
+CommandButtonControl.prototype.submitSpecialInput = function () { };
+
+CommandButtonControl.prototype.submitRawInput = function () { };
+
+function ChordButtonControl(keys, x, y, callback) {
+	this.keys = keys;
+	this.x = x;
+	this.y = y;
+	this.callback = callback || function () { };
+}
+
+ChordButtonControl.prototype.draw = function () {
+	var totalWidth = 100;
+	if (currentText.length > 0 && this.label.indexOf(currentText) === 0) {
+		draw.setColor(0x00ff00);
+	} else {
+		draw.setColor(0xffffff);
+	}
+	draw.rect(this.x, this.y, totalWidth + 2, 16 + 2);
+	draw.setColor(0x101010);
+	for (var x = 0; x < 9; x++) {
+		draw.rect(this.x + 1, this.y + 1, 11, 16);
+	}
+	draw.rect(this.x + 1, this.y + 1, totalWidth, 16);
+	draw.setColor(0xffffff);
+};
+
+ChordButtonControl.prototype.submitInput = function () {
+	if (this.label !== currentText) return;
+	this.callback();
+};
+
+ChordButtonControl.prototype.submitSpecialInput = function () { };
+
+ChordButtonControl.prototype.submitRawInput = function () { };
 
 function ProgressBar(width, step, x, y, callback) {
 	this.active = false;
@@ -106,24 +144,26 @@ ProgressBar.prototype.submitInput = function () { };
 
 ProgressBar.prototype.submitSpecialInput = function () { };
 
+ProgressBar.prototype.submitRawInput = function () { };
+
 function addProgressBarPlusButton(label, x, y, callback) {
 	var progressBar = new ProgressBar(250, 30, x + 110, y, function () {
 
 	});
-	controls.push(new ButtonControl(label, x, y, function () {
+	controls.push(new ChordButtonControl(label, x, y, function () {
 		progressBar.active = !progressBar.active;
 		if (!progressBar.active) { progressBar.currentWidth = 0; }
 	}));
 	controls.push(progressBar);
 }
 
-addProgressBarPlusButton("a", 40, 20);
-addProgressBarPlusButton("b", 40, 40);
-addProgressBarPlusButton("c", 40, 60);
-addProgressBarPlusButton("d", 40, 80);
-addProgressBarPlusButton("e", 40, 100);
-addProgressBarPlusButton("f", 40, 120);
-addProgressBarPlusButton("g", 40, 140);
+addProgressBarPlusButton("xa", 40, 20);
+addProgressBarPlusButton("xb", 40, 40);
+addProgressBarPlusButton("xc", 40, 60);
+addProgressBarPlusButton("xd", 40, 80);
+addProgressBarPlusButton("xe", 40, 100);
+addProgressBarPlusButton("xf", 40, 120);
+addProgressBarPlusButton("xg", 40, 140);
 
 sys.on("draw", "controlPanel_draw", function (e) {
 	controls.forEach(function (control) {
